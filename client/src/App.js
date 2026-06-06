c462b3bde17f74247cffbc421610002fa86c4197
muraripeetambaram8-cell <muraripeetambaram8@gmail.com>
2026-06-06 12:20:47 +0530

    force rebuild v3

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,22 @@
+// Build timestamp: 2024-06-06-v3
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
+
+function App() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/chat" /> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={user ? <Chat /> : <Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
